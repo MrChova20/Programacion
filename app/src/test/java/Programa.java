416524003dd9f7162dc2504b4aab4b6ce5dfe59a
Pablo <pchoapa@upv.edu.es>
2021-10-08 10:05:14 +0200

    Initial commit

---
+++ app/src/test/java/Programa.java
@@ -0,0 +1,11 @@
+public class Programa {
+    static Long PROGRAMACION[] = {6, 10, 12, 15, 17, 20, 22, 24};
+    static String TIPO[] = {"NOTICIAS", "MAGAZINE", "MUSICA", "NOTICIAS",
+            "MAGAZINE", "NOTICIAS", "MAGAZINE", "MUSICA"};
+
+    private long horaDeInicio()
+
+    private long horaFinal()
+
+
+}
